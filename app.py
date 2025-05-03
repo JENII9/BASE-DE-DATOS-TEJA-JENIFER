f4b253078085b79600cbe3c739ddf96b520fe41d
JENII9 <jeniferramirezibarra@gmail.com>
2025-05-03 10:26:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ CORS(app)
 # MongoDB Atlas URI desde variable de entorno
 MONGO_URI = os.environ.get("MONGO_URI")
 client = MongoClient(MONGO_URI)
-db = client["BasePryEsp32"]
-collection = db["Datos"]
+db = client["sensores"]
+collection = db["datos"]
 
 
 
